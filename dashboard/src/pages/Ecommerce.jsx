2820078c9f45908c04c64b4bb3b89eec5e994cee
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-14 21:37:34 +0800

    added 2nd feature Carts

--- dashboard/src/pages/Ecommerce.jsx
+++ dashboard/src/pages/Ecommerce.jsx
@@ -12,30 +12,55 @@ import { useStateContext } from "../contexts/ContextProvider";
 const Ecommerce = () => {
   return (
     <div className="mt-12">
+      {/* first section ROW*/}
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
-        {/* first section the earnings */}
+        {/* first section 1st feature Earning*/}
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
-              <p className="text-2xl font-semibold" >$1,000,000</p>
+              <p className="text-2xl font-semibold">$1,000,000</p>
             </div>
           </div>
           <div className="mt-6">
-            <Button 
-              color='white'
-              bgColor='red'
-              text='Download'
-              borderRadius='10px'
-              size='md'
+            <Button
+              color="white"
+              bgColor="red"
+              text="Download"
+              borderRadius="10px"
+              size="md"
             />
           </div>
         </div>
 
-        {/* second section the carts */}
-        <div>
-
+        {/* first section 2nd feature Carts*/}
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+            >
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-lg"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+            </div>
+          ))}
         </div>
+      </div>
+      
+      {/* Second section ROW */}
+      <div>
 
       </div>
     </div>
